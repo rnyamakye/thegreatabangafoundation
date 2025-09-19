2f2ed2e42f087a4d3b62db68afa6ffbf8afd5724
Richard Nyamekye <rnya2022@gmail.com>
2025-09-19 09:12:31 +0000

    added vercel speed insights

--- thegreatabangafoundation/app/root.tsx
+++ thegreatabangafoundation/app/root.tsx
@@ -7,6 +7,7 @@ import {
   ScrollRestoration,
 } from "react-router";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import type { Route } from "./+types/root";
 import { Navbar } from "./components/Navbar";
@@ -82,6 +83,7 @@ export default function App() {
       <Footer />
       <SocialMediaFixed />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
